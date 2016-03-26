c6ade3ec776f0cc1f8d99c578e4832379b3b8c82
jonas2515 <jonas2515@users.noreply.github.com>
2016-03-26 22:27:40 +0100

    primou: Update wifi board configuration
    * Allow bcmdhd wifi preallocation
    * Add get_country_code function

--- arch/arm/mach-msm/board-primou-wifi.c
+++ arch/arm/mach-msm/board-primou-wifi.c
@@ -9,11 +9,7 @@
 #include <asm/gpio.h>
 #include <asm/io.h>
 #include <linux/skbuff.h>
-#ifdef CONFIG_BCMDHD_GOOGLE
-#include <linux/wlan_plat.h>
-#else
 #include <linux/wifi_tiwlan.h>
-#endif
 
 #include "board-primou.h"
 
@@ -22,27 +18,34 @@ int primou_wifi_reset(int on);
 int primou_wifi_set_carddetect(int on);
 int primou_wifi_get_mac_addr(unsigned char *buf);
 
-#define PREALLOC_WLAN_NUMBER_OF_SECTIONS	4
-#define PREALLOC_WLAN_NUMBER_OF_BUFFERS		160
-#define PREALLOC_WLAN_SECTION_HEADER		24
+#define WLAN_STATIC_SCAN_BUF0		5
+#define WLAN_STATIC_SCAN_BUF1		6
+#define PREALLOC_WLAN_SEC_NUM		4
+#define PREALLOC_WLAN_BUF_NUM		160
+#define PREALLOC_WLAN_SECTION_HEADER	24
 
-#define WLAN_SECTION_SIZE_0	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 128)
-#define WLAN_SECTION_SIZE_1	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 128)
-#define WLAN_SECTION_SIZE_2	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 512)
-#define WLAN_SECTION_SIZE_3	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 1024)
+#define WLAN_SECTION_SIZE_0	(PREALLOC_WLAN_BUF_NUM * 128)
+#define WLAN_SECTION_SIZE_1	(PREALLOC_WLAN_BUF_NUM * 128)
+#define WLAN_SECTION_SIZE_2	(PREALLOC_WLAN_BUF_NUM * 512)
+#define WLAN_SECTION_SIZE_3	(PREALLOC_WLAN_BUF_NUM * 1024)
 
-#define WLAN_SKB_BUF_NUM	16
+#define DHD_SKB_HDRSIZE		336
+#define DHD_SKB_1PAGE_BUFSIZE	((PAGE_SIZE * 1) - DHD_SKB_HDRSIZE)
+#define DHD_SKB_2PAGE_BUFSIZE	((PAGE_SIZE * 2) - DHD_SKB_HDRSIZE)
+#define DHD_SKB_4PAGE_BUFSIZE	((PAGE_SIZE * 4) - DHD_SKB_HDRSIZE)
 
-//#define HW_OOB 1
+#define WLAN_SKB_BUF_NUM	17
 
 static struct sk_buff *wlan_static_skb[WLAN_SKB_BUF_NUM];
+static void *wlan_static_scan_buf0;
+static void *wlan_static_scan_buf1;
 
-typedef struct wifi_mem_prealloc_struct {
+struct wlan_mem_prealloc {
 	void *mem_ptr;
 	unsigned long size;
-} wifi_mem_prealloc_t;
+};
 
-static wifi_mem_prealloc_t wifi_mem_array[PREALLOC_WLAN_NUMBER_OF_SECTIONS] = {
+static struct wlan_mem_prealloc wlan_mem_array[PREALLOC_WLAN_SEC_NUM] = {
 	{ NULL, (WLAN_SECTION_SIZE_0 + PREALLOC_WLAN_SECTION_HEADER) },
 	{ NULL, (WLAN_SECTION_SIZE_1 + PREALLOC_WLAN_SECTION_HEADER) },
 	{ NULL, (WLAN_SECTION_SIZE_2 + PREALLOC_WLAN_SECTION_HEADER) },
@@ -51,31 +54,251 @@ static wifi_mem_prealloc_t wifi_mem_array[PREALLOC_WLAN_NUMBER_OF_SECTIONS] = {
 
 static void *primou_wifi_mem_prealloc(int section, unsigned long size)
 {
-	if (section == PREALLOC_WLAN_NUMBER_OF_SECTIONS)
+	if (section == PREALLOC_WLAN_SEC_NUM)
 		return wlan_static_skb;
-	if ((section < 0) || (section > PREALLOC_WLAN_NUMBER_OF_SECTIONS))
+	if (section == WLAN_STATIC_SCAN_BUF0)
+		return wlan_static_scan_buf0;
+	if (section == WLAN_STATIC_SCAN_BUF1)
+		return wlan_static_scan_buf1;
+	if ((section < 0) || (section > PREALLOC_WLAN_SEC_NUM))
 		return NULL;
-	if (wifi_mem_array[section].size < size)
+
+	if (wlan_mem_array[section].size < size)
 		return NULL;
-	return wifi_mem_array[section].mem_ptr;
+
+	return wlan_mem_array[section].mem_ptr;
 }
 
 int __init primou_init_wifi_mem(void)
 {
 	int i;
+	int j;
 
-	for (i = 0 ; (i < WLAN_SKB_BUF_NUM) ; i++) {
-		if (i < (WLAN_SKB_BUF_NUM/2))
-			wlan_static_skb[i] = dev_alloc_skb(4096);
-		else
-			wlan_static_skb[i] = dev_alloc_skb(8192);
+	for (i = 0; i < 8; i++) {
+		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_1PAGE_BUFSIZE);
+		if (!wlan_static_skb[i])
+			goto err_skb_alloc;
 	}
-	for (i = 0 ; (i < PREALLOC_WLAN_NUMBER_OF_SECTIONS) ; i++) {
-		wifi_mem_array[i].mem_ptr = kmalloc(wifi_mem_array[i].size, GFP_KERNEL);
-		if (wifi_mem_array[i].mem_ptr == NULL)
-			return -ENOMEM;
+	for (; i < 16; i++) {
+		wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_2PAGE_BUFSIZE);
+		if (!wlan_static_skb[i])
+			goto err_skb_alloc;
 	}
+	wlan_static_skb[i] = dev_alloc_skb(DHD_SKB_4PAGE_BUFSIZE);
+	if (!wlan_static_skb[i])
+		goto err_skb_alloc;
+
+	for (i = 0; i < PREALLOC_WLAN_SEC_NUM; i++) {
+		wlan_mem_array[i].mem_ptr =
+			kmalloc(wlan_mem_array[i].size, GFP_KERNEL);
+
+		if (!wlan_mem_array[i].mem_ptr)
+			goto err_mem_alloc;
+	}
+	wlan_static_scan_buf0 = kmalloc(65536, GFP_KERNEL);
+	if (!wlan_static_scan_buf0)
+		goto err_mem_alloc;
+	wlan_static_scan_buf1 = kmalloc(65536, GFP_KERNEL);
+	if (!wlan_static_scan_buf1)
+		goto err_mem_alloc;
+
 	return 0;
+
+err_mem_alloc:
+	pr_err("Failed to mem_alloc for wifi\n");
+	for (j = 0; j < i; j++)
+		kfree(wlan_mem_array[j].mem_ptr);
+
+	i = WLAN_SKB_BUF_NUM;
+
+err_skb_alloc:
+	pr_err("Failed to skb_alloc for wifi\n");
+	for (j = 0; j < i; j++)
+		dev_kfree_skb(wlan_static_skb[j]);
+
+	return -ENOMEM;
+}
+
+
+/* Customized Locale table : OPTIONAL feature */
+#define COUNTRY_BUF_SZ	4
+struct cntry_locales_custom {
+	char iso_abbrev[COUNTRY_BUF_SZ];
+	char custom_locale[COUNTRY_BUF_SZ];
+	int  custom_locale_rev;
+};
+
+static struct cntry_locales_custom primou_wifi_translate_custom_table[] = {
+/* Table should be filled out based on custom platform regulatory requirement */
+	{"",   "XV", 17},	/* Universal if Country code is unknown or empty */
+	{"IR", "XV", 17},	/* Universal if Country code is IRAN, (ISLAMIC REPUBLIC OF) */
+	{"SD", "XV", 17},	/* Universal if Country code is SUDAN */
+	{"SY", "XV", 17},	/* Universal if Country code is SYRIAN ARAB REPUBLIC */
+	{"GL", "XV", 17},	/* Universal if Country code is GREENLAND */
+	{"PS", "XV", 17},	/* Universal if Country code is PALESTINE */
+	{"TL", "XV", 17},	/* Universal if Country code is TIMOR-LESTE (EAST TIMOR) */
+	{"MH", "XV", 17},	/* Universal if Country code is MARSHALL ISLANDS */
+	{"PK", "XV", 17},	/* Universal if Country code is PAKISTAN */
+	{"CK", "XV", 17},	/* Universal if Country code is Cook Island (13.4.27)*/
+	{"CU", "XV", 17},	/* Universal if Country code is Cuba (13.4.27)*/
+	{"FK", "XV", 17},	/* Universal if Country code is Falkland Island (13.4.27)*/
+	{"FO", "XV", 17},	/* Universal if Country code is Faroe Island (13.4.27)*/
+	{"GI", "XV", 17},	/* Universal if Country code is Gibraltar (13.4.27)*/
+	{"IM", "XV", 17},	/* Universal if Country code is Isle of Man (13.4.27)*/
+	{"CI", "XV", 17},	/* Universal if Country code is Ivory Coast (13.4.27)*/
+	{"JE", "XV", 17},	/* Universal if Country code is Jersey (13.4.27)*/
+	{"KP", "XV", 17},	/* Universal if Country code is North Korea (13.4.27)*/
+	{"FM", "XV", 17},	/* Universal if Country code is Micronesia (13.4.27)*/
+	{"MM", "XV", 17},	/* Universal if Country code is Myanmar (13.4.27)*/
+	{"NU", "XV", 17},	/* Universal if Country code is Niue (13.4.27)*/
+	{"NF", "XV", 17},	/* Universal if Country code is Norfolk Island (13.4.27)*/
+	{"PN", "XV", 17},	/* Universal if Country code is Pitcairn Islands (13.4.27)*/
+	{"PM", "XV", 17},	/* Universal if Country code is Saint Pierre and Miquelon (13.4.27)*/
+	{"SS", "XV", 17},	/* Universal if Country code is South_Sudan (13.4.27)*/
+	{"AL", "AL", 2},
+	{"DZ", "DZ", 1},
+	{"AS", "AS", 12},	/* changed 2 -> 12*/
+	{"AI", "AI", 1},
+	{"AG", "AG", 2},
+	{"AR", "AR", 21},
+	{"AW", "AW", 2},
+	{"AU", "AU", 6},
+	{"AT", "AT", 4},
+	{"AZ", "AZ", 2},
+	{"BS", "BS", 2},
+	{"BH", "BH", 4},	/* changed 24 -> 4*/
+	{"BD", "BD", 2},
+	{"BY", "BY", 3},
+	{"BE", "BE", 4},
+	{"BM", "BM", 12},
+	{"BA", "BA", 2},
+	{"BR", "BR", 4},
+	{"VG", "VG", 2},
+	{"BN", "BN", 4},
+	{"BG", "BG", 4},
+	{"KH", "KH", 2},
+	{"CA", "CA", 31},
+	{"KY", "KY", 3},
+	{"CN", "CN", 24},
+	{"CO", "CO", 17},
+	{"CR", "CR", 17},
+	{"HR", "HR", 4},
+	{"CY", "CY", 4},
+	{"CZ", "CZ", 4},
+	{"DK", "DK", 4},
+	{"EE", "EE", 4},
+	{"ET", "ET", 2},
+	{"FI", "FI", 4},
+	{"FR", "FR", 5},
+	{"GF", "GF", 2},
+	{"DE", "DE", 7},
+	{"GR", "GR", 4},
+	{"GD", "GD", 2},
+	{"GP", "GP", 2},
+	{"GU", "GU", 12},
+	{"HK", "HK", 2},
+	{"HU", "HU", 4},
+	{"IS", "IS", 4},
+	{"IN", "IN", 3},
+	{"ID", "KR", 25},	/* ID/1 -> KR/24 */
+	{"IE", "IE", 5},
+	{"IL", "BO", 0},	/* IL/7 -> BO/0 */
+	{"IT", "IT", 4},
+	{"JP", "JP", 58},
+	{"JO", "JO", 3},
+	{"KW", "KW", 5},
+	{"LA", "LA", 2},
+	{"LV", "LV", 4},
+	{"LB", "LB", 5},
+	{"LS", "LS", 2},
+	{"LI", "LI", 4},
+	{"LT", "LT", 4},
+	{"LU", "LU", 3},
+	{"MO", "MO", 2},
+	{"MK", "MK", 2},
+	{"MW", "MW", 1},
+	{"MY", "MY", 3},
+	{"MV", "MV", 3},
+	{"MT", "MT", 4},
+	{"MQ", "MQ", 2},
+	{"MR", "MR", 2},
+	{"MU", "MU", 2},
+	{"YT", "YT", 2},
+	{"MX", "MX", 20},
+	{"MD", "MD", 2},
+	{"MC", "MC", 1},
+	{"ME", "ME", 2},
+	{"MA", "MA", 2},
+	{"NP", "NP", 3},
+	{"NL", "NL", 4},
+	{"AN", "AN", 2},
+	{"NZ", "NZ", 4},
+	{"NO", "NO", 4},
+	{"OM", "OM", 4},
+	{"PA", "PA", 17},
+	{"PG", "PG", 2},
+	{"PY", "PY", 2},
+	{"PE", "PE", 20},
+	{"PH", "PH", 5},
+	{"PL", "PL", 4},
+	{"PT", "PT", 4},
+	{"PR", "PR", 20},
+	{"RE", "RE", 2},
+	{"RO", "RO", 4},
+	{"SN", "SN", 2},
+	{"RS", "RS", 2},
+	{"SG", "SG", 4},
+	{"SK", "SK", 4},
+	{"SI", "SI", 4},
+	{"ES", "ES", 4},
+	{"LK", "LK", 1},
+	{"SE", "SE", 4},
+	{"CH", "CH", 4},
+	{"TW", "TW", 1},
+	{"TH", "TH", 5},
+	{"TT", "TT", 3},
+	{"TR", "TR", 7},
+	{"AE", "AE", 6},
+	{"UG", "UG", 2},
+	{"GB", "GB", 6},
+	{"UY", "UY", 1},
+	{"VI", "VI", 13},
+	{"VA", "VA", 2},
+	{"VE", "VE", 3},
+	{"VN", "VN", 4},
+	{"MA", "MA", 1},
+	{"ZM", "ZM", 2},
+	{"EC", "EC", 21},
+	{"SV", "SV", 19},
+	{"KR", "KR", 57},
+	{"RU", "RU", 13},
+	{"UA", "UA", 8},
+	{"GT", "GT", 1},
+	{"MN", "MN", 1},
+	{"NI", "NI", 2},
+	{"US", "Q2", 57},
+};
+
+static void *primou_wifi_get_country_code(char *ccode)
+{
+	int size, i;
+	static struct cntry_locales_custom country_code;
+
+	size = ARRAY_SIZE(primou_wifi_translate_custom_table);
+
+	if ((size == 0) || (ccode == NULL))
+		return NULL;
+
+	for (i = 0; i < size; i++) {
+		if (!strcmp(
+			ccode, primou_wifi_translate_custom_table[i].iso_abbrev))
+			return &primou_wifi_translate_custom_table[i];
+	}
+
+	memset(&country_code, 0, sizeof(struct cntry_locales_custom));
+	strlcpy(country_code.custom_locale, ccode, COUNTRY_BUF_SZ);
+
+	return &country_code;
 }
 
 static struct resource primou_wifi_resources[] = {
@@ -83,11 +306,7 @@ static struct resource primou_wifi_resources[] = {
 		.name		= "bcmdhd_wlan_irq",
 		.start		= MSM_GPIO_TO_INT(PRIMOU_GPIO_WIFI_IRQ),
 		.end		= MSM_GPIO_TO_INT(PRIMOU_GPIO_WIFI_IRQ),
-#ifdef CONFIG_BCMDHD_GOOGLE
-		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHEDGE,
-#else
-		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHEDGE,
-#endif
+		.flags		= IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHEDGE,
 	},
 };
 
@@ -97,16 +316,17 @@ static struct wifi_platform_data primou_wifi_control = {
 	.set_carddetect = primou_wifi_set_carddetect,
 	.mem_prealloc   = primou_wifi_mem_prealloc,
 	.get_mac_addr	= primou_wifi_get_mac_addr,
+	.get_country_code	= primou_wifi_get_country_code,
 };
 
 static struct platform_device primou_wifi_device = {
-				.name           = "bcmdhd_wlan",
-				.id             = 1,
-				.num_resources  = ARRAY_SIZE(primou_wifi_resources),
-				.resource       = primou_wifi_resources,
-				.dev            = {
-				.platform_data = &primou_wifi_control,
-				},
+	.name           = "bcmdhd_wlan",
+	.id             = 1,
+	.num_resources  = ARRAY_SIZE(primou_wifi_resources),
+	.resource       = primou_wifi_resources,
+	.dev            = {
+		.platform_data = &primou_wifi_control,
+	},
 };
 
 unsigned char *get_wifi_nvs_ram(void);
@@ -139,57 +359,6 @@ static unsigned primou_wifi_update_nvs(char *str)
 	return 0;
 }
 
-#ifdef HW_OOB
-static unsigned strip_nvs_param(char *param)
-{
-	unsigned char *nvs_data;
-
-	unsigned param_len;
-	int start_idx, end_idx;
-
-	unsigned char *ptr;
-	unsigned len;
-
-	if (!param)
-		return -EINVAL;
-	ptr = get_wifi_nvs_ram();
-	/* Size in format LE assumed */
-	memcpy(&len, ptr + NVS_LEN_OFFSET, sizeof(len));
-
-	/* the last bye in NVRAM is 0, trim it */
-	if (ptr[NVS_DATA_OFFSET + len - 1] == 0)
-		len -= 1;
-
-	nvs_data = ptr + NVS_DATA_OFFSET;
-
-	param_len = strlen(param);
-
-	/* search param */
-	for (start_idx = 0; start_idx < len - param_len; start_idx++) {
-		if (memcmp(&nvs_data[start_idx], param, param_len) == 0)
-			break;
-	}
-
-	end_idx = 0;
-	if (start_idx < len - param_len) {
-		/* search end-of-line */
-		for (end_idx = start_idx + param_len; end_idx < len; end_idx++) {
-			if (nvs_data[end_idx] == '\n' || nvs_data[end_idx] == 0)
-				break;
-		}
-	}
-
-	if (start_idx < end_idx) {
-		/* move the remain data forward */
-		for (; end_idx + 1 < len; start_idx++, end_idx++)
-			nvs_data[start_idx] = nvs_data[end_idx+1];
-
-		len = len - (end_idx - start_idx + 1);
-		memcpy(ptr + NVS_LEN_OFFSET, &len, sizeof(len));
-	}
-	return 0;
-}
-#endif
 
 #define WIFI_MAC_PARAM_STR     "macaddr="
 #define WIFI_MAX_MAC_LEN       17 /* XX:XX:XX:XX:XX:XX */
@@ -266,11 +435,7 @@ int __init primou_wifi_init(void)
 	int ret;
 
 	printk(KERN_INFO "%s: start\n", __func__);
-#ifdef HW_OOB
-	strip_nvs_param("sd_oobonly");
-#else
 	primou_wifi_update_nvs("sd_oobonly=1\n");
-#endif
 	primou_wifi_update_nvs("btc_params80=0\n");
 	primou_wifi_update_nvs("btc_params6=30\n");
 	primou_init_wifi_mem();
