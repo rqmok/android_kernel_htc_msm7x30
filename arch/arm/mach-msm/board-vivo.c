7d0a6681e943ee3d123e70e1adc2aa5bd5c6c6b6
Zeeshan <rqmok@hotmail.com>
2016-07-18 16:30:22 +0930

    vivo: Use shared heap for mm and vidc.
    Change-Id: I5646dca250fb4af3dcc9fce49675a5a90c351189

--- arch/arm/mach-msm/board-vivo.c
+++ arch/arm/mach-msm/board-vivo.c
@@ -4331,6 +4331,7 @@ static void __init size_pmem_devices(void)
 {
 #ifdef CONFIG_ANDROID_PMEM
 #ifndef CONFIG_ANDROID_PMEM_ION_WRAPPER
+	android_pmem_adsp_pdata.start = MSM_PMEM_ADSP_BASE;
 	android_pmem_adsp_pdata.size = MSM_PMEM_ADSP_SIZE;
 #endif
 #endif
@@ -4341,7 +4342,6 @@ static void __init reserve_pmem_memory(void)
 #ifdef CONFIG_ANDROID_PMEM
 #ifndef CONFIG_ANDROID_PMEM_ION_WRAPPER
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += PMEM_KERNEL_EBI0_SIZE;
-	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_PMEM_ADSP_SIZE;
 #endif
 #endif
 }
@@ -4349,6 +4349,7 @@ static void __init reserve_pmem_memory(void)
 static void __init size_ion_devices(void)
 {
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
+	ion_pdata.heaps[1].base = MSM_PMEM_ADSP_BASE;
 	ion_pdata.heaps[1].size = MSM_ION_MM_SIZE;
 	ion_pdata.heaps[2].size = MSM_ION_SF_SIZE;
 	ion_pdata.heaps[3].size = MSM_ION_WB_SIZE;
@@ -4358,7 +4359,6 @@ static void __init size_ion_devices(void)
 static void __init reserve_ion_memory(void)
 {
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
-  	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_MM_SIZE;
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_SF_SIZE;
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_WB_SIZE;
 #endif
